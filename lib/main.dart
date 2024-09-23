861c8a418186d12d73cc2b5c7e704acd3c502d94
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2024-09-23 22:42:17 +0700

    expense tracker

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:expense_tracker/expenses.dart';
+import 'package:expense_tracker/widgets/expenses.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
+      theme: ThemeData(useMaterial3: true),
       home: Expenses(),
     ),
   );
