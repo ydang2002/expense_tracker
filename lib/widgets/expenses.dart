92421da8bed3ccfa068a8cb4cd6ea60dd3c70f2c
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2024-09-25 23:59:46 +0700

    DropdownButton & _submitExpenseDate

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -28,9 +28,17 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
+      isScrollControlled: true,
       context: context,
-      builder: (ctx) => const NewExpense(),
+      builder: (ctx) => NewExpense(onAddExpense: _addExpense),
     );
+  }
+
+  void _addExpense(Expense expense) {
+    setState(() {
+
+    });
+    _registeredExpenses.add(expense);
   }
 
   @override
