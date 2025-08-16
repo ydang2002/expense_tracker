cd3ff04e722988f308a58d5a0786530e5173ab81
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2025-08-16 08:50:08 +0700

    test commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:expense_tracker/widgets/expenses.dart';
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData(useMaterial3: true),//test commit
       home: Expenses(),
     ),
   );
